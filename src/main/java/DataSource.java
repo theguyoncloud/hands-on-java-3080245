e07f060da497b496fe2841b27ca3eed66db60a71
thecloudguy <18654888+theguyoncloud@users.noreply.github.com>
2023-10-29 11:21:00 +0000

    get account info method in data source class

--- src/main/java/DataSource.java
+++ src/main/java/DataSource.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import bank.Account;
 import bank.Customer;
 
 public class DataSource {
@@ -47,10 +48,33 @@ public class DataSource {
     return customer;
   }
 
+  public static Account getAccount(int accountId){
+    String sql = "select * from accounts where id = ?";
+    Account account = null;
+    try(Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)){
+          statement.setInt(1, accountId);
+
+          try(ResultSet resultSet = statement.executeQuery()){
+            account = new Account(
+              resultSet.getInt("id"),
+              resultSet.getString("type"),
+              resultSet.getDouble("balance"));
+          }
+        }catch(SQLException e){
+          e.printStackTrace();
+        }
+
+        return account;
+
+  }
+
   public static void main(String[] args) {
     Customer customer = getCustomer("twest8o@friendfeed.com");
+    Account account = getAccount(customer.getAccountId());
     System.out.println(customer.getName());
     System.out.println(customer.getAccountId());
+    System.out.println(account.getBalance());
   }
 
 }
